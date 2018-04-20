4836191ae688d33c72fcee96e7ce431de263764f
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2018-04-20 19:31:45 +0300

    Добавил SQL

---
+++ BSearch/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿Select Distinct [Name], [Position] from [Employed] where [Departament_Id] = 1 order by [Name] 
+
+SELECT DISTINCT a.[Name], b.[Position]
+FROM (SELECT [Name] FROM [Employed] where [Departament_Id] = 1) AS a  CROSS JOIN (SELECT [Position] FROM [Employed]  where [Departament_Id] = 1) AS b;
+Select a.[Name], a.[Salary] from [Employed] as a where a.[Salary] >= 2 * (select AVG(g.[Salary]) from [Employed] as g where g.Chief_Id = a.Id)
